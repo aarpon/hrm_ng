9d975b9c4fd3c79dbf36e1c67e91a812ba42d6d3
Aaron Ponti <aarpon@gmail.com>
2015-06-24 16:35:13 +0200

    Extend functionality of setup/setup.sh.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
 
-# Get current folder
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#
+# Set project directory
+#
+PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
+
+#
+# Composer
+#
+
+# Make sure all third-party dependencies exist and are up-to-date
+if [ ! -d "${PROJECT_DIR}/vendor" ]; then
+    # Install
+    ${PROJECT_DIR}/composer.phar install
+else
+    # Update
+    ${PROJECT_DIR}/composer.phar update
+fi
 
 #
 # Propel
 #
 
+# Make sure the credentials file exists
+if [ ! -f "${PROJECT_DIR}/config/db/propel.yaml" ]; then
+    # Copy the sample file
+    cp ${PROJECT_DIR}/config/db/propel.yaml.sample ${PROJECT_DIR}/config/db/propel.yaml
+
+    # Inform and leave
+    echo "Please add the database credentials to config/db/propel.yaml and restart."
+    exit 0
+fi
+
 # Generate SQL
-${DIR}/../vendor/bin/propel sql:build  --overwrite \
-    --config-dir "${DIR}/../config/db" \
-    --schema-dir "${DIR}/../config/db" \
-    --output-dir "${DIR}/../config/db"
+${PROJECT_DIR}vendor/bin/propel sql:build  --overwrite \
+    --config-dir "${PROJECT_DIR}config/db" \
+    --schema-dir "${PROJECT_DIR}config/db" \
+    --output-dir "${PROJECT_DIR}config/db"
 
 # Generate classes
-${DIR}/../vendor/bin/propel model:build \
-    --config-dir "${DIR}/../config/db" \
-    --schema-dir "${DIR}/../config/db" \
-    --output-dir="${DIR}/../src"
+${PROJECT_DIR}vendor/bin/propel model:build \
+    --config-dir "${PROJECT_DIR}config/db" \
+    --schema-dir "${PROJECT_DIR}config/db" \
+    --output-dir="${PROJECT_DIR}src"
 
 # Runtime connection settings
-${DIR}/../vendor/bin/propel config:convert \
-    --config-dir "${DIR}/../config/db" \
-    --output-dir="${DIR}/../src"
+${PROJECT_DIR}vendor/bin/propel config:convert \
+    --config-dir "${PROJECT_DIR}config/db" \
+    --output-dir="${PROJECT_DIR}src"
 
 # This will import the schema to the database. It is assumed that
 # the database was already created and that the user configured in
 # config/propel.yaml has the right to create the tables. Please mind
 # that this will drop any existing table! Schema migration has not
 # been implemented yet!
-${DIR}/../vendor/bin/propel sql:insert \
-    --config-dir "${DIR}/../config/db" \
-    --sql-dir "${DIR}/../config/db"
+${PROJECT_DIR}vendor/bin/propel sql:insert \
+    --config-dir "${PROJECT_DIR}config/db" \
+    --sql-dir "${PROJECT_DIR}config/db"
