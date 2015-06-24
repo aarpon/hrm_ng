2c7bfa4f94481cda8b57bb7c6e32f2e6fda40fc5
Aaron Ponti <aarpon@gmail.com>
2015-06-24 11:38:58 +0200

    Add application bootstrapping.

---
+++ src/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+// Set up Composer's autoloading
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+
+// Set up Propel
+require_once dirname(__FILE__) . '/config.php';
+
+// Set up logging
+// TODO
+
+// Settings
+// TODO
