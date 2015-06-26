c142fcb5b5dae5beb4d36e2c2a19e9aa63c3a518
Aaron Ponti <aarpon@gmail.com>
2015-06-26 19:29:40 +0200

    The column 'name' of table 'user' has 'unique' constraint. Added more tests.

--- test/UserTest.php
+++ test/UserTest.php
@@ -1,9 +1,22 @@
 <?php
 
+/**
+ * UserTest.php
+ *
+ * This test suite checks the User class and database table.
+ */
+
 // Bootstrap
 require_once dirname(__FILE__) . '/../src/bootstrap.php';
 
 class UserTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var userID: the ID we will be using in the test,
+     *              in case the database already contains
+     *             data.
+     */
+    protected $userID;
 
     /**
      * Test instantiation of the \hrm\User class
@@ -12,6 +25,26 @@ class UserTest extends PHPUnit_Framework_TestCase {
     {
         $user = new \hrm\User();
         $this->assertTrue($user !== null);
+    }
+
+
+    /**
+     * Test deleting all users from the database.
+     *
+     * This is not really a test. We just need the
+     * User table to be empty at the beginning of the
+     * test series.
+     */
+    public function testDeleteAllUsers()
+    {
+        # Create a UserQuery to retrieve a collection
+        # of all User objects
+        $users = \hrm\UserQuery::create()->find();
+
+        # Look for the user we just added.
+        foreach ($users as $user) {
+            $user->delete();
+        }
     }
 
     /**
@@ -27,6 +60,7 @@ class UserTest extends PHPUnit_Framework_TestCase {
         $user->setPassword("TestPassword");
         $user->setEmail("test@email.com");
         $user->setResearchGroup("TestGroup");
+        $user->setAuthentication("active_dir");
         $user->setRole("user");
         $user->setCreationDate(new DateTime());
         $user->setLastAccessDate(new DateTime());
@@ -35,5 +69,83 @@ class UserTest extends PHPUnit_Framework_TestCase {
         # Save user
         $this->assertTrue($user->save() > 0);
 
+        # Store its primary key and store it
+        $this->userID = $user->getPrimaryKey();
+
+        # Return the userID to be used in other tests
+        return $this->userID;
+    }
+
+    /**
+     * Test adding a user with duplicate name to the database.
+     *
+     * The name column is unique, therefore this must fail.
+     *
+     * @covers \hrm\User::save
+     * @expectedException \Propel\Runtime\Exception\PropelException
+     */
+    public function testDuplicateUser()
+    {
+        $this->setExpectedException("\\Propel\\Runtime\\Exception\\PropelException");
+
+        # Create user
+        $user = new \hrm\User();
+
+        # Set all properties (the name is the same)
+        $user->setName("TestUser");
+        $user->setPassword("DifferentPassword");
+        $user->setEmail("different@email.com");
+        $user->setResearchGroup("differentGroup");
+        $user->setAuthentication("ldap");
+        $user->setRole("admin");
+        $user->setCreationDate(new DateTime());
+        $user->setLastAccessDate(new DateTime());
+        $user->setStatus("disabled");
+
+        # Save user: this will throw an expected exception
+        # and will count as a test success.
+        $user->save();
+    }
+
+    /**
+     * Test retrieving the added user
+     */
+    public function testRetrieveUserByName()
+    {
+        # Create a UserQuery to retrieve a collection
+        # of all User objects
+        $users= \hrm\UserQuery::create()->find();
+
+        # Look for the user we just added.
+        foreach ($users as $user) {
+            if ($user->getPrimaryKey() == $this->userID) {
+                # Check the name
+                $this->assertTrue(
+                    ($user->getName() == "TestUser") &&
+                    ($user->getStatus() == "active"));
+            }
+        }
+
+    }
+
+    /**
+     * Test retrieving the added user by UserQuery
+     *
+     * This needs to retrieve the userID we stored in testAddUser().
+     *
+     * @depends testAddUser
+     */
+    public function testRetrieveUserByPrimaryKey($userID)
+    {
+        # We need to store $userID again
+        $this->userID = $userID;
+
+        # Create a UserQuery
+        $q = new \hrm\UserQuery();
+        $user = $q->findPK($this->userID);
+        $this->assertTrue($user !== null);
+
+        # Check the name
+        $this->assertTrue($user->getName() == "TestUser");
     }
 }
