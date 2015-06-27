c93ffa0d4fdd1fb62f1be4d944b04b8f1cee0037
Aaron Ponti <aarpon@gmail.com>
2015-06-27 18:03:35 +0200

    Add composer self-update call to setup/setup.sh.

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,6 +9,9 @@ PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../
 # Composer
 #
 
+# Update composer itself
+${PROJECT_DIR}/composer.phar self-update
+
 # Make sure all third-party dependencies exist and are up-to-date
 if [ ! -d "${PROJECT_DIR}/vendor" ]; then
     # Install
