cff578def1545834a24bd456ead416822cd6c901
Aaron Ponti <aarpon@gmail.com>
2015-06-27 17:49:02 +0200

    Add propel.yaml.dist file versions for MySQL and PostgreSQL.

--- setup/setup.sh
+++ setup/setup.sh
@@ -32,22 +32,29 @@ if [ ! -f "${PROJECT_DIR}/config/db/propel.yaml" ]; then
     exit 0
 fi
 
+# Make sure the propel.yaml.dist file exists
+if [ ! -f "${PROJECT_DIR}/config/db/propel.yaml.dist" ]; then
+    # Inform and leave
+    echo "Please copy one of config/db/propel.yaml.dist_{mysql|pgsql} to config/db/propel.yaml.dist and restart."
+    exit 0
+fi
+
 # Generate SQL
-${PROJECT_DIR}vendor/bin/propel sql:build  --overwrite \
-    --config-dir "${PROJECT_DIR}config/db" \
-    --schema-dir "${PROJECT_DIR}config/db" \
-    --output-dir "${PROJECT_DIR}config/db"
+${PROJECT_DIR}/vendor/bin/propel sql:build  --overwrite \
+    --config-dir "${PROJECT_DIR}/config/db" \
+    --schema-dir "${PROJECT_DIR}/config/db" \
+    --output-dir "${PROJECT_DIR}/config/db"
 
 # Generate classes
-${PROJECT_DIR}vendor/bin/propel model:build \
-    --config-dir "${PROJECT_DIR}config/db" \
-    --schema-dir "${PROJECT_DIR}config/db" \
-    --output-dir="${PROJECT_DIR}src"
+${PROJECT_DIR}/vendor/bin/propel model:build \
+    --config-dir "${PROJECT_DIR}/config/db" \
+    --schema-dir "${PROJECT_DIR}/config/db" \
+    --output-dir="${PROJECT_DIR}/src"
 
 # Runtime connection settings
 ${PROJECT_DIR}vendor/bin/propel config:convert \
-    --config-dir "${PROJECT_DIR}config/db" \
-    --output-dir="${PROJECT_DIR}src"
+    --config-dir "${PROJECT_DIR}/config/db" \
+    --output-dir="${PROJECT_DIR}/src"
 
 # This will import the schema to the database. It is assumed that
 # the database was already created and that the user configured in
@@ -55,5 +62,5 @@ ${PROJECT_DIR}vendor/bin/propel config:convert \
 # that this will drop any existing table! Schema migration has not
 # been implemented yet!
 ${PROJECT_DIR}vendor/bin/propel sql:insert \
-    --config-dir "${PROJECT_DIR}config/db" \
-    --sql-dir "${PROJECT_DIR}config/db"
+    --config-dir "${PROJECT_DIR}/config/db" \
+    --sql-dir "${PROJECT_DIR}/config/db"
