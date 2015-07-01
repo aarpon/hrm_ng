80d761b6c383c485269d7f503c9e30c88d9d3b86
Aaron Ponti <aarpon@gmail.com>
2015-07-01 12:40:04 +0200

    Set up HRM logger based on monolog/monolog.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,7 +7,20 @@ require_once dirname(__FILE__) . '/../vendor/autoload.php';
 require_once dirname(__FILE__) . '/config.php';
 
 // Set up logging
-// TODO
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// Temporary log folder for development. Later, replace
+// it with a configurable location.
+$log_folder = dirname(__FILE__) . '/../log';
+
+$HRM_LOGGER = new Logger('hrm');
+$HRM_LOGGER->pushHandler(new StreamHandler(
+        $log_folder . '/development.log',
+        Logger::DEBUG));
+$HRM_LOGGER->pushHandler(new StreamHandler(
+        $log_folder . 'log/production.log',
+        Logger::WARNING));
 
 // Settings
 // TODO
