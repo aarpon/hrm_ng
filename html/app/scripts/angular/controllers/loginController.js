e41a3fe7700e6e6934646e4ff77b5f52efa41af8
Olivier Burri <olivier.burri@gmail.com>
2015-07-06 00:59:05 +0200

    modal new user form

--- html/app/scripts/angular/controllers/loginController.js
+++ html/app/scripts/angular/controllers/loginController.js
@@ -1,11 +1,7 @@
 /**
  * Created by oburri on 02.07.15.
  */
-hrmapp.controller('loginController', function($scope, $rootScope, authService, AUTH_EVENTS, LOG_EVENTS, uiLogService) {
-    $scope.user = {
-        username: '',
-        password: ''
-    };
+hrmapp.controller('loginController', function($scope, $rootScope, $mdDialog, authService, AUTH_EVENTS, LOG_EVENTS, uiLogService) {
 
     $scope.loginUser = function(credentials) {
         authService.loginUser(credentials).then(function (user) {
@@ -16,4 +12,29 @@ hrmapp.controller('loginController', function($scope, $rootScope, authService, A
             uiLogService.addLog(LOG_EVENTS.err, errorMessage)
         });
     };
+
+    $scope.showNewUserForm = function(ev) {
+        $mdDialog.show({
+            controller: newUserFormController,
+            templateUrl: 'templates/new-user.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+        })
+            .then(function(newuserinfo) {
+                $scope.newuser = newuserinfo;
+            }, function() {
+                $scope.alert = newuserinfo;
+            });
+    };
 });
+function newUserFormController($scope, $mdDialog) {
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+    };
+}
