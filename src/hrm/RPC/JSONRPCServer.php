42e95b61808b2712574d60631f5f01e0f3d5f9c8
Aaron Ponti <aarpon@gmail.com>
2015-07-20 16:30:08 +0200

    Add JSONRPCServer and SessionManager classes.
    The JSONRPCSever and SessionManager classes re-structure and encapsulate the functionality that was scattered in the json-rpc-server.php script.

--- src/hrm/RPC/JSONRPCServer.php
+++ src/hrm/RPC/JSONRPCServer.php
@@ -1,26 +1,85 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aaron
- * Date: 17/07/15
- * Time: 19:38
- */
+/*
+
+Server implementing the JSON-RPC (version 2.0) protocol.
+
+This is an example Javascript code to interface with json-rpc-server.php:
+
+01:    <script type="text/javascript">
+02:        $(document).ready($('#button').click(function() {
+03:            JSONRPCRequest({
+04:                method : 'isLoggedIn',
+05:                params : {userName : 'name'}
+06:            }, function(data) {
+07:                $('#report').html("<b>" + data['result'] + "</b>");
+08:            });
+09:        }));
+10:    </script>
+
+Passing parameters to the Ajax method is very flexible (line 5). The recommended method is:
+
+                  params : {parameter : 'value'}
+
+for one parameter, and:
+
+                  params : {parameterOne : 'valueOne',
+                            parameterTwo : 'valueTwo'}
+
+for more parameters. If a parameter is an array, use:
+
+
+                  params : {parameter : ['valueOne', 'valueTwo', 'valueThree']}
+
+In PHP, the parameters can then be retrieved with:
+
+                  $params = $_POST['params'];
+
+===
+
+For illustration, the following is also possible:
+
+For a single value:
+
+Javascript:   params : 'ExcitationWavelength'
+PHP:          $params := "ExcitationWavelength"
+
+For a vector:
+
+Javascript:   params : ['ExcitationWavelength', 'EmissionWavelength']
+PHP:          $params[0] := "ExcitationWavelength"
+              $params[1] := "EmissionWavelength"
+
+*/
 
 namespace hrm\RPC;
+use hrm\User\Base\UserQuery;
 
+require_once dirname(__FILE__) . '/../../bootstrap.php';
 
+/**
+ * Class JSONRPCServer SIMPLE JSON RPC Server class.
+ * @package hrm\RPC
+ */
 class JSONRPCServer
 {
+    /**
+     * @var $sessionManager \hrm\RPC\SessionManager Simple session manager.
+     */
+    private $sessionManager;
 
     /**
      * @var string HTTP response status
      */
-    private $httpReponseStatus = "200 OK";
+    private $httpResponseStatus = "400 Bad Request";
 
     /**
-     * @var string HTTP response content (JSON array).
+     * @var Array HTTP response content.
      */
-    private $httpResponseContent = "";
+    private $httpResponseArray = array(
+        "id" => -1,
+        "result" => array(),
+        "success" => false,
+        "message" => "");
 
     /**
      * @var string Response content type: "application/json;charset=utf8"
@@ -28,14 +87,10 @@ class JSONRPCServer
     private $httpResponseContentType = "application/json;charset=utf8";
 
     /**
-     * @var string JSON RPC request sent from the client.
+     * @var Array JSON RPC request sent from the client (converted to a PHP
+     * array).
      */
-    private $httpRequest = "";
-
-    /**
-     * @var string Last error message.
-     */
-    private $lastError = "";
+    private $httpRequest = null;
 
     /**
      * @var bool Flag that indicates whether the JSON RPC request is valid.
@@ -48,11 +103,6 @@ class JSONRPCServer
     private $clientId = -1;
 
     /**
-     * @var string PHP session ID on the server as returned by php_session_id().
-     */
-    private $phpSessionId = "";
-
-    /**
      * @var string Invoked method.
      */
     private $method = "";
@@ -63,17 +113,50 @@ class JSONRPCServer
     private $params = null;
 
     /**
-     * Constructor
-     * @param $request string JSON RPC request from the client.
-     * @param $phpSessionID string Currently active session ID.
+     * @var array User roles and their numeric mappings:
+     *
+     * user: 0
+     * manager: 1
+     * admin: 2
      */
-    public function __construct($request, $phpSessionID)
+    private static $USER_ROLES = array(
+        "user"    => 0,
+        "manager" => 1,
+        "admin"   => 2
+    );
+
+    /**
+     * @const
+     * @var array Array of known and valid methods with the minimum user
+     *            role required for running it.§
+     */
+    private static $VALID_METHODS = array(
+        "logIn"   => 0,           // Everyone
+        "logOut"  => 0,           // Everyone
+        "addUser" => 1);          // Manager or higher
+
+    /**
+     * Constructor
+     * @param $request Array JSON RPC request from the client converted to
+     * a PHP array.
+     * @param $phpSessionID string Currently active session ID.
+     *
+     * The session ID must be retrieved from $_SESSION and passed
+     * on as argument to the constructor.
+     */
+    public function __construct(array $request)
     {
+        // Instantiate the SessionManager
+        $this->sessionManager = new SessionManager();
+
+        // Make sure the $request is an array
+        if (is_string($request))
+        {
+            $request = json_decode($request);
+        }
+
         // Store the request
         $this->httpRequest = $request;
-
-        // Store the PHP session ID
-        $this->phpSessionId = $phpSessionID;
 
         // Validate it
         $this->validateRequest();
@@ -84,7 +167,17 @@ class JSONRPCServer
      */
     public function executeRequest()
     {
+        // Call the method
+        $this->{$this->method}();
+    }
 
+    /**
+     * Returns the validity of the request.
+     * @return bool True if the request is valid, false otherwise.
+     */
+    public function isRequestValid()
+    {
+        return $this->isRequestValid;
     }
 
     /**
@@ -103,7 +196,7 @@ class JSONRPCServer
      */
     public function getResponseStatus()
     {
-        return $this->httpReponseStatus;
+        return $this->httpResponseStatus;
     }
 
     /**
@@ -122,7 +215,7 @@ class JSONRPCServer
      */
     public function getResponseBody()
     {
-        return $this->httpReponseStatus;
+        return json_encode($this->httpResponseArray);
     }
 
     /**
@@ -158,7 +251,7 @@ class JSONRPCServer
      */
     public function getResponseContentLength()
     {
-        return mb_strlen($this->httpResponseContent);
+        return mb_strlen(json_encode($this->httpResponseArray));
     }
 
     /**
@@ -166,18 +259,19 @@ class JSONRPCServer
      */
     private function validateRequest()
     {
+        // Make sure the request valid status is false
+        $this->isRequestValid = false;
+
         // Do we have a JSON-RPC 2.0 request?
         if (!(array_key_exists('jsonrpc', $this->httpRequest) &&
             $this->httpRequest['jsonrpc'] == "2.0")) {
 
-            // Invalid JSON-RPC 2.0 call
-            $this->lastError = "Invalid JSON-RPC 2.0 call.";
-
             // Flag invalid
             $this->isRequestValid = false;
 
-            // TODO Set the response code
-            $this->httpReponseStatus = "400 <INCOMPLETE>";
+            // Set the response to failure
+            $this->setFailure(-1, array(), "Invalid JSON-RPC 2.0 call.",
+                "400 Bad Request");
 
             // Return here
             return;
@@ -194,8 +288,9 @@ class JSONRPCServer
             // Flag invalid
             $this->isRequestValid = false;
 
-            // TODO Set the response code
-            $this->httpReponseStatus = "400 <INCOMPLETE>";
+            // Set the response to failure
+            $this->setFailure(-1, array(), "Session ID is missing!",
+                "400 Bad Request");
 
             // Return here
             return;
@@ -207,30 +302,12 @@ class JSONRPCServer
         // The method is mandatory
         if (!isset($this->httpRequest['method'])) {
 
-            // Expected method
-            $this->lastError = "Expected method!";
-
             // Flag invalid
             $this->isRequestValid = false;
 
-            // TODO Set the response code
-            $this->httpReponseStatus = "400 <INCOMPLETE>";
-
-            // Return here
-            return;
-        }
-
-        // Check that the method is valid
-        if (! $this->isMethodValid())
-        {
-            // Invalid method
-            $this->lastError = "Invalid method!";
-
-            // Flag invalid
-            $this->isRequestValid = false;
-
-            // TODO Set the response code
-            $this->httpReponseStatus = "400 <INCOMPLETE>";
+            // Set the response to failure
+            $this->setFailure(-1, array(), "Method missing!",
+                "400 Bad Request");
 
             // Return here
             return;
@@ -238,6 +315,20 @@ class JSONRPCServer
 
         // Store the method
         $this->method = $this->httpRequest['method'];
+
+        // Check that the method is valid
+        if (! $this->isMethodValid())
+        {
+            // Flag invalid
+            $this->isRequestValid = false;
+
+            // Set the response to failure
+            $this->setFailure(-1, array(), "Invalid method!",
+                "400 Bad Request");
+
+            // Return here
+            return;
+        }
 
         // Method parameters
         $this->params = null;
@@ -249,19 +340,20 @@ class JSONRPCServer
             // Che that the parameters are valid
             if (! $this->areParametersValid())
             {
-                // Invalid parameters
-                $this->lastError = "Invalid parameters!";
-
                 // Flag invalid
                 $this->isRequestValid = false;
 
-                // TODO Set the response code
-                $this->httpReponseStatus = "400 <INCOMPLETE>";
+                // Set the response to failure
+                $this->setFailure(-1, array(), "Invalid parameters!",
+                    "400 Bad Request");
 
                 // Return here
                 return;
             }
         }
+
+        // Now the valid status can be set to true
+        $this->isRequestValid = true;
 
     }
 
@@ -271,16 +363,217 @@ class JSONRPCServer
      */
     private function isMethodValid()
     {
-        return true;
+        return (array_key_exists($this->method, self::$VALID_METHODS));
     }
 
     /**
      * Check if the parameters for currently set method are valid.
      * @return bool True if the parameters are valid, false otherwise.
+     * @throws Exception if the requested method is invalid. This should not
+     * happen, since the call to areParametersValid() should follow a call to
+     * isMethodValid().
      */
     private function areParametersValid()
     {
-        return true;
+        // Check the parameters for current method
+        switch ($this->method) {
+
+            case "logIn":
+
+                // logIn needs 'username' and 'password'
+                return (array_key_exists('username', $this->params) &&
+                    array_key_exists('password', $this->params));
+
+            case "logOut":
+
+                // logOut only needs a valid client ID
+                return $this->clientId != -1;
+
+            case "addUser":
+
+                // Make sure the expected parameters exist
+                // TODO: Complete
+                return false;
+
+            default:
+
+                // Unknown method. This is a bug!
+                throw new Exception("Invalid method requested!");
+        }
+    }
+
+    /**
+     * Check whether currently logged in User has the permissions to run
+     * the requested method.
+     * @return bool True if the user can run the method, false otherwise.
+     */
+    private function userCanRunMethod()
+    {
+        // The logIn and logOut methods can always be run
+        if ($this->methodName == "logIn" || $this->methodName == "logOut") {
+            return true;
+        }
+
+        // Check it the UserID is stored in the session
+        $userID = $this->sessionManager->get('UserID');
+        if ($userID !== null) {
+
+            // Try retrieving the user
+            $user = UserQuery::create()->findOneById($userID);
+            if (null === $user) {
+                return false;
+            }
+
+            // Get User role
+            $role = $user->getRole();
+
+            if ($role < self::$USER_ROLES[$this->method]) {
+                return false;
+            }
+
+            return true;
+        }
+
+        // If the user does not exist, the method cannot be run.
+        return false;
+    }
+
+    /**
+     * Fills the response array in case of success.
+     *
+     * Please mind that a success is defined as a successful execution of the
+     * requested method. For example, if the user credentials are wrong in an
+     * attempt to login the user, the method will return success, even though
+     * the result of the login attempt will be negative.
+     *
+     * @param $id string|integer Session ID
+     * @param $result Array Result from the method call.
+     * @param $message string Message returned by the method call.
+     * @param string $responseStatus HTTP/1.1 response status. Default for
+     *               success is "200 OK".
+     * @return Array JSON object.
+     */
+    private function setSuccess($id, $result = array(), $message = "",
+                                $responseStatus = "200 OK")
+    {
+        // Initialize and fill the array
+        $this->httpResponseArray = array(
+            "id" => $id,
+            "success" => true,
+            "result" => $result,
+            "message" => $message);
+
+        // Set the response code
+        $this->httpResponseStatus = $responseStatus;
+    }
+
+    /**
+     * Fills the response array in case of failure.
+     *
+     * @see setSuccess for a definition of success and failure of a method call.
+     *
+     * @param $id string|integer Session ID
+     * @param $result Array Result from the method call.
+     * @param $message string Message returned by the method call.
+     * @param string $responseStatus HTTP/1.1 response status. Default for
+     *               failure is "400 Bad Request".
+     * @return Array JSON object.
+     */
+    function setFailure($id, $result = array(), $message = "",
+                        $responseStatus = "400 Bad Request")
+    {
+        // Initialize and fill the array
+        $this->httpResponseArray = array(
+            "id" => $id,
+            "success" => false,
+            "result" => $result,
+            "message" => $message);
+
+        // Set the response code
+        $this->httpResponseStatus = $responseStatus;
+    }
+
+    /*
+     *
+     * METHOD IMPLEMENTATIONS
+     *
+     */
+    private function logIn()
+    {
+        // Prepare the result
+        $result = array("success" => null, "role" => null);
+
+        // Query the User
+        $user = UserQuery::create()->findOneByName($this->params["username"]);
+        if (null === $user) {
+
+            // Fill the result array
+            $result["success"] = false;
+            $result["role"] = null;
+
+            // The User does not exist!
+            $this->setFailure(-1, $result, "The user does not exist.");
+
+        }
+
+        // Try authenticating the user.
+        if ($user->logIn($this->params["password"])) {
+
+            // Start a new session
+            $this->sessionManager->restart();
+
+            // Fill in the result array
+            $result["success"] = true;
+            $result["role"] = $user->getRole();
+
+            // Successful login
+            // TODO: Set http response status
+            $this->setSuccess($this->sessionManager->getSessionID(), $result,
+                "The user was logged in successfully.");
+
+            // Store the User ID in the PHP session
+            $this->sessionManager->set('UserID', $user->getId());
+
+        } else {
+
+            // Fill in the result array
+            $result["success"] = false;
+            $result["role"] = null;
+
+            // Set success (although the user could not be authenticated).
+            // TODO: Set http response status
+            $this->setSuccess(-1, $result,
+                "The user could not be logged in.");
+
+        }
+    }
+
+    /**
+     * Log out current User
+     * @return bool True if the User could be logged out; false otherwise.
+     */
+    private function logOut()
+    {
+        // Is the session active?
+        if (! $this->sessionManager->isActive($this->clientId))
+        {
+            // The session was not active and/or no User was logged in.
+            $this->setFailure(-1, false,
+                $this->sessionManager->lastMessage(),
+                "400 Bad request");
+
+        } else {
+
+            // Destroy current session
+            $this->sessionManager->destroy();
+
+            // Report success
+            $this->setSuccess(-1, true,
+                "The user was logged out successfully.",
+                "200 OK");
+
+        }
     }
 
 }
+
